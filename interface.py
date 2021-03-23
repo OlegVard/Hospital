f4cd1844ce22803e14dcef20464303350c25ab1c
OlegVard <61628812+OlegVard@users.noreply.github.com>
2021-03-23 13:27:56 +0700

    making interface

--- interface.py
+++ interface.py
@@ -1,18 +1,39 @@
 # this file need to create an interface for my app
 import tkinter as tk
 
-class main_window(tk.Frame):
+
+class MainWindow(tk.Frame):
     def __init__(self, root):
         super().__init__(root)
         self.init_main()
 
     def init_main(self):
+        toolbar = tk.Frame(bg='#d7d8e0', bd=2)
+        toolbar.pack(side=tk.TOP, fill=tk.X)
+        btn_open_dialog = tk.Button(toolbar,
+                                    text='Записаться на прием',
+                                    command= lambda: self.open_dialog(),
+                                    compound=tk.TOP,)
+        btn_open_dialog.pack(side=tk.LEFT)
 
+    def open_dialog(self):
+        self.Child()
 
+    class Child(tk.Toplevel):
+        def __init__(self):
+            super().__init__(root)
+            self.init_child()
+
+        def init_child(self):
+            self.title('Запись на прием')
+            self.geometry("500x300+200+100")
+            self.resizable(False, False)
+            self.grab_set()
+            self.focus_get()
 
 if __name__ == "__main__":
     root = tk.Tk()
-    app = main_window(root)
+    app = MainWindow(root)
     app.pack()
     root.title("Hospital")
     root.geometry("700x500+300+200")
