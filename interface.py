b773d18ebcffa8fc305255ac54e028d147a3079a
OlegVard <61628812+OlegVard@users.noreply.github.com>
2021-03-23 13:06:44 +0700

    making interface

--- interface.py
+++ interface.py
@@ -1,1 +1,20 @@
 # this file need to create an interface for my app
+import tkinter as tk
+
+class main_window(tk.Frame):
+    def __init__(self, root):
+        super().__init__(root)
+        self.init_main()
+
+    def init_main(self):
+
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = main_window(root)
+    app.pack()
+    root.title("Hospital")
+    root.geometry("700x500+300+200")
+    root.resizable(True, True)
+    root.mainloop()
